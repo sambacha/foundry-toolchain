ebd366c1b2a5b2f3150e0ca05e7c02e6d2da0d21
sam bacha <sam@manifoldfinance.com>
2022-05-09 07:42:36 -0700

    Create output.test.js

---
+++ test/output.test.js
@@ -0,0 +1,13 @@
+// @testfile
+const OutputListener = require('../src/OutputListener');
+
+describe('output-listener', () => {
+  it('receives and exposes data', () => {
+    const listener = new OutputListener();
+    const listen = listener.listener;
+    listen(Buffer.from('foo'));
+    listen(Buffer.from('bar'));
+    listen(Buffer.from('baz'));
+    expect(listener.contents).toEqual('foobarbaz');
+  });
+});
